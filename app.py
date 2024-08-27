c15950e4ba1ddbee2c41c6954a97cd278c3b09aa
SwayamBhatt <156022829+SwayamBhatt@users.noreply.github.com>
2024-08-27 20:55:23 +0530

    app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ st.title("Motor vehicle collisions in New York City")
 st.markdown("This application is a Streamlit dashboard that can be used "
             "to analyze motor vehicle collisions in NYC 🗽💥🚗")
 
-@st.cache_data(persist=True)
+@st.cache_data(persist=False)
 def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows = nrows, parse_dates = ['CRASH_DATE', 'CRASH_TIME'])
+    data = pd.read_csv(DATA_URL, nrows = nrows, parse_dates = [['CRASH_DATE', 'CRASH_TIME']])
     data.dropna(subset = ['LATITUDE', 'LONGITUDE'], inplace = True)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
