d3f3abc6960cd9009283502f4ed134310cc2d284
SwayamBhatt <156022829+SwayamBhatt@users.noreply.github.com>
2024-08-27 20:56:42 +0530

    app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.markdown("This application is a Streamlit dashboard that can be used "
 
 @st.cache_data(persist=False)
 def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows = nrows, parse_dates = [['CRASH_DATE', 'CRASH_TIME']])
+    data = pd.read_csv(DATA_URL, nrows = nrows, 'parse_dates': 'CRASH_DATE, CRASH_TIME')
     data.dropna(subset = ['LATITUDE', 'LONGITUDE'], inplace = True)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
